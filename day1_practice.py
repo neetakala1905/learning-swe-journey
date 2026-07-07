4adc5688f3797170a7517b00f395fb77beaf6ef1
neetakala1905 <neetakala57@gmail.com>
2026-07-07 20:17:40 +0530

    commiting

---
+++ day1_practice.py
@@ -0,0 +1,27 @@
+for y in range(6):
+  print("hi", y)
+  
+print("break")
+for x in range(1, 6):
+    print(x)
+    
+print("new")
+for z in range(3, 30, 4):
+  print("*",z)
+  
+print("else block")
+for a in range(1, 6):
+  print("&")
+else:
+  print("Finished")
+  
+  
+print("if-else")
+for b in range(6):
+  if b == 4: 
+    print("we got it", b)
+    break
+  
+  
+  else:
+    print(b)
